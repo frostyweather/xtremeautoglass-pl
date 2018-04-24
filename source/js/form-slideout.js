76b6d366d5f2f3b155e5b8e845114846082c8690
Ian Frost <inf5000@gmail.com>
2018-04-24 19:08:58 -0400

    SVG stuff. Form slideout stuff

--- source/js/form-slideout.js
+++ source/js/form-slideout.js
@@ -6,57 +6,59 @@
  * 1)
  */
 (function() {
-	if (document.querySelector(".js-get-quote")) {
-		var quoteButton = document.querySelector(".js-get-quote");
-		var formCloseTrigger = document.querySelector(".js-form-close-trigger");
+	if (document.querySelector(".js-form-slideout")) {
+		if (document.querySelectorAll(".js-get-quote")) {
+			var quoteButton = document.querySelector(".js-get-quote");
+			var formCloseTrigger = document.querySelector(".js-form-close-trigger");
 
-		quoteButton.addEventListener("click", function(event) {
-			event.preventDefault();
-			var formSlideout = document.querySelector(".js-form-slideout");
-			if (formSlideout.classList.contains("is-active")) {
-				formSlideout.classList.remove("is-active");
+			quoteButton.addEventListener("click", function(event) {
+				event.preventDefault();
+				var formSlideout = document.querySelector(".js-form-slideout");
+				if (formSlideout.classList.contains("is-active")) {
+					formSlideout.classList.remove("is-active");
+					document.body.classList.remove("is-disabled");
+				} else {
+					formSlideout.classList.add("is-active");
+					document.body.classList.add("is-disabled");
+				}
+			});
+
+			formCloseTrigger.addEventListener("click", function(event) {
+				event.preventDefault();
 				document.body.classList.remove("is-disabled");
-			} else {
-				formSlideout.classList.add("is-active");
-				document.body.classList.add("is-disabled");
-			}
-		});
 
-		formCloseTrigger.addEventListener("click", function(event) {
-			event.preventDefault();
-			document.body.classList.remove("is-disabled");
+				this.parentNode.parentNode.classList.remove("is-active");
+			});
+		}
 
-			this.parentNode.parentNode.classList.remove("is-active");
-		});
-	}
-
-	var radioTrigger = document.querySelectorAll(".js-form-dropdown input[type='radio']");
-	for (i=0; i<radioTrigger.length; i++) {
-		radioTrigger[i].addEventListener("click", function(event) {
-			if(this.nextElementSibling.innerText == "Yes") {
-				var hiddenFields = document.querySelectorAll(".js-form-dropdown-target");
-				for (j=0; j<hiddenFields.length; j++) {
-					hiddenFields[j].classList.remove("u-is-hidden");
+		var radioTrigger = document.querySelectorAll(".js-form-dropdown input[type='radio']");
+		for (i=0; i<radioTrigger.length; i++) {
+			radioTrigger[i].addEventListener("click", function(event) {
+				if(this.nextElementSibling.innerText == "Yes") {
+					var hiddenFields = document.querySelectorAll(".js-form-dropdown-target");
+					for (j=0; j<hiddenFields.length; j++) {
+						hiddenFields[j].classList.remove("u-is-hidden");
+					}
 				}
-			}
-			else {
-				var hiddenFields = document.querySelectorAll(".js-form-dropdown-target");
-				for (j=0; j<hiddenFields.length; j++) {
-					hiddenFields[j].classList.add("u-is-hidden");
+				else {
+					var hiddenFields = document.querySelectorAll(".js-form-dropdown-target");
+					for (j=0; j<hiddenFields.length; j++) {
+						hiddenFields[j].classList.add("u-is-hidden");
+					}
 				}
-			}
-		});
+			});
+		}
+
+		var stepHeading = document.querySelector(".js-step-2");
+
+		stepHeading.classList.add("u-is-hidden");
+
+		var nextButton = document.querySelector(".js-next-button");
+		nextButton.classList.remove("u-is-hidden");
+		nextButton.addEventListener("click", function(event) {
+			stepHeading.classList.remove("u-is-hidden");
+			this.parentNode.removeChild(nextButton);
+		})
 	}
-
-	var stepHeading = document.querySelector(".js-step-2");
-
-	stepHeading.classList.add("u-is-hidden");
-
-	var nextButton = document.querySelector(".js-next-button");
-	nextButton.classList.remove("u-is-hidden");
-	nextButton.addEventListener("click", function(event) {
-		stepHeading.classList.remove("u-is-hidden");
-		this.parentNode.removeChild(nextButton);
-	})
 
 })();
